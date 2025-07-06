762f102582a817106b53e2ae05c78efb7c6d5558
HeniRidene <heniridene77@gmail.com>
2025-07-06 12:50:40 +0100

    init

---
+++ auth/connexion.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$host = 'localhost';
+$dbname = 'hanouty';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    // Set PDO in exception mode
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    // Handle database connection error
+    echo "Connection failed: " . $e->getMessage();
+}
